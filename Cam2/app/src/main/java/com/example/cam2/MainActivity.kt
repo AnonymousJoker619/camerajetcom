534d0869bd40dfcae441c10b4df4d75c0e122faa
AnonymousJoker619 <71007411+AnonymousJoker619@users.noreply.github.com>
2022-10-04 23:50:34 +0530

    f

---
+++ Cam2/app/src/main/java/com/example/cam2/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.cam2
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(onClick = {
+                    val navigate = Intent(this@MainActivity,SecondActivity::class.java)
+                    startActivity(navigate)
+                }) {
+                    Text(text = "Camera", fontSize = 18.sp)
+                }
+            }
+        }
+    }
+}
